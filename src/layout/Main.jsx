76c19c83239d2e27e6a72369affd90c93fb89bca
iibrahim70 <aviknuhash70@gmail.com>
2023-05-18 08:00:33 +0600

    add the footer section and firebase config

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Navbar from '../pages/shared/Navbar';
+import Footer from '../pages/shared/Footer';
+import { Outlet } from 'react-router-dom';
 
 const Main = () => {
   return (
     <div>
       <Navbar/>
+      <div className='w-90% mx-auto'>
+        <Outlet/>
+      </div>
+      <Footer/>
     </div>
   );
 };
