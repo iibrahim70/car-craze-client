9f37365470f4538eef469d1928b5f99925144d88
iibrahim70 <iibrahiim.dev@gmail.com>
2023-05-18 11:05:31 +0600

    implemented log in page

---
+++ src/pages/login/Signup.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Signup = () => {
+  return (
+    <div className='my-10 grid grid-cols-2 gap-x-5 min-h-screen'>
+      <div className=' bg-orange-200'>
+        <div className='bg-red-200 grid items-center'>
+          <p>ami</p>
+          <p>ami</p>
+          <p>tumi </p>
+          <p>helo</p>
+        </div>
+      </div>
+      <div className=' bg-orange-600'>kire</div>
+    </div>
+  );
+};
+
+export default Signup;
+
