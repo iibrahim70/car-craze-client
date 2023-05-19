1fb2a84701b524d0442b4782a6245de735a027de
iibrahim70 <iibrahiim.dev@gmail.com>
2023-05-19 18:34:39 +0600

    added galary section of the webpage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from '../../components/homepage/Hero';
+import Galary from '../../components/homepage/Galary';
 
 const Home = () => {
   return (
     <>
       <Hero/>
+      <Galary/>
     </>
   );
 };
