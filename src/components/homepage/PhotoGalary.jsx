b28470b21556523acab5b347b8fd1ee4db2d56be
iibrahim70 <aviknuhash70@gmail.com>
2023-05-20 08:39:03 +0600

    update the photo galary

--- src/components/homepage/PhotoGalary.jsx
+++ src/components/homepage/PhotoGalary.jsx
@@ -4,6 +4,9 @@ import sportsCar2 from '../../assets/sports car/2.jpg'
 import sportsCar3 from '../../assets/sports car/3.jpg'
 import sportsCar4 from '../../assets/sports car/4.jpg'
 import miniTruck1 from '../../assets/mini truck/1.jpg'
+import miniTruck2 from '../../assets/mini truck/2.jpg'
+import miniTruck3 from '../../assets/mini truck/3.jpg'
+import miniTruck4 from '../../assets/mini truck/4.jpg'
 
 const PhotoGalary = () => {
   return (
@@ -36,16 +39,16 @@ const PhotoGalary = () => {
           <p>Blaze Buster</p>
         </div>
         <div className="space-y-2 text-center">
-          <img className='md:h-60' src={sportsCar1} alt="" />
-          <p>Ferrari 488 GTB</p>
+          <img className='md:h-60' src={miniTruck2} alt="" />
+          <p>Spark Responder</p>
         </div>
         <div className="space-y-2 text-center">
-          <img className='md:h-60' src={sportsCar3} alt="" />
-          <p>Audi R8 Spyder</p>
+          <img className='md:h-60' src={miniTruck3} alt="" />
+          <p>Mini Inferno</p>
         </div>
         <div className="space-y-2 text-center">
-          <img className='md:h-60' src={sportsCar4} alt="" />
-          <p>Lamborghini Aventador</p>
+          <img className='md:h-60' src={miniTruck4} alt="" />
+          <p>Firefly</p>
         </div>
         
       </div>
