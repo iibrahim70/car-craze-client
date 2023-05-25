81891c5abedfbcb7e5e84c13b3d3adff7c1b8621
iibrahim70 <iibrahiim.dev@gmail.com>
2023-05-25 13:26:37 +0600

    fix the update related issue and customize the scrollbar

--- src/components/homepage/Hero.jsx
+++ src/components/homepage/Hero.jsx
@@ -9,14 +9,14 @@ const Hero = () => {
     Aos.init({duration: 1000});
   }, [])
   return (
-    <div data-aos='fade-up' className='mt-[104px] mb-10 md:mt-0 md:mb-0'>
+    <div className='mt-[104px] mb-10 md:mt-0 md:mb-0'>
       <div className="grid grid-cols-1 md:grid-cols-2 min-h-screen mt-[48px]">
-        <div className="flex flex-col justify-center space-y-5">
+        <div data-aos='fade-up' className="flex flex-col justify-center space-y-5">
           <h1 className="text-3xl md:text-6xl font-bold">Car <span className='text-[#2ECC71]'>Craze</span> - <br /> Toy Car Site</h1>
           <p className='text-justify md:w-2/3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet veritatis nobis libero nemo iure dolor.</p>
           <button className='btn-primary w-[35%] md:w-[22%]'>Find Toys</button>
         </div>
-        <div className="flex flex-col justify-center items-center">
+        <div data-aos='fade-down' className="flex flex-col justify-center items-center">
           <Lottie animationData={carAnimation} loop={true} />
         </div>
       </div>
