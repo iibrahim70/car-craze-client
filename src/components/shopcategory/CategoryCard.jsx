2f34bd5d19a8deef1adfa1ca38dae4e3178ad3bd
iibrahim70 <iibrahiim.dev@gmail.com>
2023-08-04 22:01:56 +0600

    added choose us section

--- src/components/shopcategory/CategoryCard.jsx
+++ src/components/shopcategory/CategoryCard.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const CategoryCard = ({category}) => {
-  const { _id, toyName, toyPicture, price, rating  } = category; 
+  const { _id, carName, toyPicture, price, rating  } = category; 
   return (
     <div className="shadow-xl">
       <figure className='h-52'><img className='h-[90%] w-full' src={toyPicture} alt="Toys" /></figure>
       <div className="ps-5 space-y-2">
-        <h2 className="font-medium">{toyName}</h2>
+        <h2 className="font-medium">{carName}</h2>
         <p>Price: ${price}</p>
         <p>Ratings: {rating}</p>
         <div className="flex justify-end pr-5 pb-2">
-          <Link to={`/toydetails/${_id}`} className="btn-primary">Details</Link>
+          <Link to={`/car-details/${_id}`} className="btn-primary">Details</Link>
         </div>
       </div>
     </div>
