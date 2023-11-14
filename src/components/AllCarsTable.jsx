89414a994db5ad7713afe5e7ed8c92cc63f4f301
iibrahim70 <aviknuhash70@gmail.com>
2023-11-14 21:02:12 +0600

    update allcars table

--- src/components/AllCarsTable.jsx
+++ src/components/AllCarsTable.jsx
@@ -2,19 +2,17 @@ import { Link } from "react-router-dom";
 import { HiArrowNarrowRight } from "react-icons/hi";
 
 const AllCarsTable = ({ item }) => {
-  const { _id, sellerName, name, category, price, quantity } = item;
-
   return (
     <>
       <tr>
-        <td>{sellerName}</td>
-        <td>{name}</td>
-        <td>{category}</td>
-        <td>${price}</td>
-        <td>{quantity}</td>
+        <td>{item?.sellerName}</td>
+        <td>{item?.name}</td>
+        <td>{item?.category}</td>
+        <td>${item?.price}</td>
+        <td>{item?.quantity}</td>
 
         <td>
-          <Link to={`/car-details/${_id}`}>
+          <Link to={`/car-details/${item?._id}`}>
             <HiArrowNarrowRight className="w-6 h-6 cursor-pointer text-[#1e212a]" />
           </Link>
         </td>
